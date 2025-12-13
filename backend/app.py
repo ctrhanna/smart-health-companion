c621ffb98f038e6d4a066d3afb47719103ee7245
ctrhanna <hannakunjumon2027@cs.ajce.in>
2025-12-13 22:04:32 +0530

    Initial Flask setup with working templates

---
+++ backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    print("Template folder:", os.path.abspath("templates"))
+    app.run(debug=True)
